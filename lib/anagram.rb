3de50d4c6891657214566592726bcf51c4ab7157
Samuel <samuel.muthaiga@student.moringaschool.com>
2023-02-10 09:18:29 +0300

    Complete

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,14 @@
 # Your code goes here!
+#BDD
+
+# pseudocode 
+class Anagram
+    def initialize(word)
+      @word = word
+    end
+  
+    def match(word_list)
+      word_list.select { |w| w.chars.sort == @word.chars.sort && w.downcase != @word.downcase }
+    end
+  end
+  
